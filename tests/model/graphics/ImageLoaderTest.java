43ad55827a5196096ddd50b42115d5be4df164e2
Onyx ab3q <Onyxab3q@outlook.com>
2022-01-08 17:11:29 +0100

    fixed tests not working because JFX uninitialized

--- tests/model/graphics/ImageLoaderTest.java
+++ tests/model/graphics/ImageLoaderTest.java
@@ -1,17 +1,21 @@
 package model.graphics;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import model.graphics.Loaders.ImageLoader;
 import model.graphics.tools.ImageConverter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static testUtilities.jfxInititer.initInternalGraphics;
 
 class ImageLoaderTest {
 
     @Test
     public void TestImageLoader(){
-        WritableImage bi = ImageConverter.ImageToWriteableImage(ImageLoader.loadRessources());
+        initInternalGraphics();
+        Image im = ImageLoader.loadRessources();
+        WritableImage bi = ImageConverter.ImageToWriteableImage(im);
         assertNotNull(bi);
     }
 }
---
+++ tests/testUtilities/jfxInititer.java
@@ -0,0 +1,9 @@
+package testUtilities;
+
+import javafx.embed.swing.JFXPanel;
+
+public class jfxInititer {
+    public static void initInternalGraphics(){
+        JFXPanel jfxPanel = new JFXPanel();
+    }
+}
