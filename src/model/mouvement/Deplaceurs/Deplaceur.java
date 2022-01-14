23c3bb5c33930bb0ee4c677c6a36c85360d66a56
Kanken6174 <yorick.geoffre@orange.fr>
2022-01-14 14:49:44 +0100

    added deplaceur proof-of-concept

--- src/model/mouvement/Deplaceurs/Deplaceur.java
+++ src/model/mouvement/Deplaceurs/Deplaceur.java
@@ -1,6 +1,10 @@
 package model.mouvement.Deplaceurs;
 
 import model.entites.Entite;
+import model.enums.Orients;
+import model.mouvement.Positions.PositionGraphique;
+import model.mouvement.Positions.PositionLogique;
+import model.terrain.Case;
 import model.terrain.EspaceDeJeu;
 
 public abstract class Deplaceur implements Runnable{
@@ -13,7 +17,20 @@ public abstract class Deplaceur implements Runnable{
     }
 
     protected void deplacerEntite(){
+        PositionLogique Posl = geree.getPositionLogique();  //quelle case
+        PositionGraphique Posg = geree.getPositionGraphique();  //offset de -4 à 4
+        if(Posg.getx() >= 4){
+            Case Destination = EJ.getCardinals(Posl).get(Orients.DROITE.ordinal()); //on teste que à droite pour le moment...
+            if(Destination == null || Destination.isObstacle())
+                return;
+            Destination.ReceiveEntity(EJ.getStage()[Posl.getCaseX()][Posl.getCaseY()].passEntity(geree));
+        }else{
+            Posg.setx(Posg.getx()+1);
+        }
+    }
 
+    protected void passeCase(Case source, Case destination){
+        destination.ReceiveEntity(source.passEntity(geree));
     }
 
     public void run(){
